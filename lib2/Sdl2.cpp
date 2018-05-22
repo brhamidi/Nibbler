93fb091b54bb38fcd3efb187ddee6bbb48f50b8c
Matthias SRUN <msrun@e1r8p7.42.fr>
2018-05-22 17:01:42 +0200

    render

--- lib2/Sdl2.cpp
+++ lib2/Sdl2.cpp
@@ -6,7 +6,7 @@
 /*   By: bhamidi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/22 12:16:39 by bhamidi           #+#    #+#             */
-/*   Updated: 2018/05/22 15:25:29 by msrun            ###   ########.fr       */
+/*   Updated: 2018/05/22 17:00:33 by msrun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,24 +33,9 @@ void	Sdl2::_init(short x, short y)
 {
 	SDL_Init(SDL_INIT_VIDEO);
 
-	this->_win = SDL_CreateWindow("Nibbler" , SDL_WINDOWPOS_CENTERED , SDL_WINDOWPOS_CENTERED , x, y, 0);
+	this->_win = SDL_CreateWindow("Nibbler" , SDL_WINDOWPOS_CENTERED , SDL_WINDOWPOS_CENTERED , x * 10 , y * 10, 0);
 
 //	SDL_Point ligne_depart,ligne_arrivee; // Déclaration du point de départ et du point d'arrivée d'une ligne
-
-
-	if(SDL_VideoInit(NULL) < 0) // Initialisation de la SDL
-	{
-		std::cerr << "Erreur d'initialisation de la SDL : " << SDL_GetError() << std::endl;
-		throw "Error";
-	}
-
-	// Création de la fenêtre :
-	this->_win = SDL_CreateWindow("Une this->_win SDL" , SDL_WINDOWPOS_CENTERED , SDL_WINDOWPOS_CENTERED , 800 , 800 , 0);
-	if(this->_win == NULL) // Gestion des erreurs
-	{
-		std::cerr << "Erreur lors de la creation d'une this->_win : " << SDL_GetError() << std::endl;
-		throw "Error";
-	}
 
 	// Création du this->_renderer :
 	this->_renderer = SDL_CreateRenderer(this->_win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC); // Création du this->_renderer
@@ -63,35 +48,31 @@ void	Sdl2::_init(short x, short y)
 
 void	Sdl2::render(Data const & data) const
 {
-    SDL_SetRenderDrawColor(this->_renderer,0,0,0,255);
-    SDL_RenderClear(this->_renderer);
-    // Le fond est à présent noir
-    
-	SDL_SetRenderDrawColor(this->_renderer,200 ,0 ,0 ,200); //Couleur blanche
-    
-	SDL_Rect cases[data._width * data._height]; // Déclaration du tableau contenant les cases blanches
-
-	// Nous allons maintenant remplir les cases blanches par dessus le fond noir
-	// Remplissage du tableau de SDL_Rect que l'on remplira ensuite avec du blanc :
-
-	int icases = 0;
+	SDL_Rect cases;
 	for (auto h = 0; h < data._height; h++)
 	{
 		for (auto w = 0; w < data._width; w++)
 		{
-			cases[icases].x = cases[icases].y = data._width;
-			cases[icases].w = cases[icases].h = data._height;
-			++icases;
+			if (data._map[h][w] == eNum::Blank)
+				SDL_SetRenderDrawColor(this->_renderer, 0, 0, 0, 200);
+			if (data._map[h][w] == eNum::Wall)
+				SDL_SetRenderDrawColor(this->_renderer, 255, 255, 0, 200);
+			if (data._map[h][w] == eNum::Snake)
+				SDL_SetRenderDrawColor(this->_renderer, 0, 255, 0, 200);
+			if (data._map[h][w] == eNum::Head)
+				SDL_SetRenderDrawColor(this->_renderer, 0, 255, 0, 200);
+			if (data._map[h][w] == eNum::Food)
+				SDL_SetRenderDrawColor(this->_renderer, 0, 0, 255, 200);
+			cases.x = w * 10;
+			cases.y = h * 10;
+			if(SDL_RenderFillRect(this->_renderer, &cases) < 0)
+			{
+				std::cerr << "Erreur lors des remplissages de rectangles: " << SDL_GetError() << std::endl;
+				throw "Error";
+			}
 		}
 	}
-
-	if(SDL_RenderFillRects(this->_renderer,cases,32) <0)//Remplissage des cases blanches
-	{
-		std::cerr << "Erreur lors des remplissages de rectangles: " << SDL_GetError() << std::endl;
-		throw "Error";
-	}
 	SDL_RenderPresent(this->_renderer);
-
 }
 
 eDir	Sdl2::getEvent(void) const
